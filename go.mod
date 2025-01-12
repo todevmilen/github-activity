5cdf035a1e98f5aa0421d37ac900e8879c8cfae4
Milen Todev <todevcode@gmail.com>
2025-01-12 22:07:56 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/todevmilen/github-activity
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+)
+
+type Event struct {
+	Id   json.Number `json:"id"`
+	Type string      `json:"type"`
+	Repo struct {
+		Id   json.Number `json:"id"`
+		Name string      `json:"name"`
+	} `json:"repo"`
+	Payload struct {
+		Size json.Number `json:"size"`
+	} `json:"payload"`
+}
+
+func main() {
+	var events []Event
+
+	if len(os.Args) <= 1 {
+		log.Fatal("Please provide username")
+	}
+
+	username := os.Args[1]
+
+	if username == "" {
+		log.Fatal("Please provide username")
+	}
+
+	url := fmt.Sprintf("https://api.github.com/users/%s/events", username)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = json.Unmarshal(body, &events)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, event := range events {
+		switch event.Type {
+		case "PushEvent":
+			fmt.Printf("- Pushed %s commits to %s\n", event.Payload.Size, event.Repo.Name)
+
+		case "ForkEvent":
+			fmt.Printf("- Forked  %s\n", event.Repo.Name)
+
+		case "WatchEvent":
+			fmt.Printf("- Starred the repository %s\n", event.Repo.Name)
+
+		default:
+			fmt.Printf("- Some Event into %s\n", event.Repo.Name)
+		}
+
+	}
+
+}
